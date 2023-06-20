8c08a867e4ced5b79b638b18961a506985289a48
Nathan Woodburn <github@nathan.woodburn.au>
2023-06-20 12:53:16 +1000

    startup: Added message on startup

---
+++ plugins/startup.js
@@ -0,0 +1,22 @@
+import events from "events";
+
+import fetch from "node-fetch";
+		
+export class Plugin {
+	constructor(bot) {
+		this.bot = bot;
+
+		this.events = new events.EventEmitter();
+
+		this.commands = [];
+		this.init();
+	}
+
+	init() {
+		// Send message to channel when bot starts
+        // wait 5 seconds to allow other plugins to load
+        setTimeout(() => {
+            this.bot.sendMessage({conversation: `5DkLWHPq`},{ message: `Bot Restarted` })
+        }, 5000);
+	}
+}
